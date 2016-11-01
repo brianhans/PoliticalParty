0da87c7182749585461fad0c566cd854425ed3ac
Madhur Malhotra <malhotra.madhur15@gmail.com>
2016-11-01 15:34:01 -0700

    Update questions.js

--- PoliticalParty/questions.js
+++ PoliticalParty/questions.js
@@ -1,34 +1,34 @@
 {
     "questions": [{
-        "question": "How many emails did Hillary Clinton delete",
+        "question": "How many emails did Hillary Clinton delete?",
         "category": "Canidates",
-        "1": "100",
-        "2": "200",
+        "1": "50,000",
+        "2": "500",
         "3": "5,000",
-        "4": "10,000,000"
+        "4": "500,000"
     }, {
-        "question": "What year was Donald Trump born?",
+        "question": "When was Donald Trump born?",
         "category": "Canidates",
         "1": "June 14, 1946",
         "2": "December 13, 1974",
         "3": "September 4, 1934",
         "4": "April 5, 1990"
     }, {
-        "question": "What year was Hillary born?",
+        "question": "When was Hillary Clinton born?",
         "category": "Canidates",
         "1": "October 26, 1947",
         "2": "December 20, 1996",
         "3": "Janurary 15, 1942",
         "4": "March 4, 1971"
     }, {
-        "question": "Who was the oldest president ever elected?",
+        "question": "Who is the oldest presidential candidate?",
         "category": "Canidates",
         "1": "Bernie Sanders",
         "2": "Hillary Clinton",
         "3": "Donald Trump",
-        "4": "Ben Carson"
+        "4": "Marco Rubio"
     }, {
-        "question": "Sanders served how many consecutive terms in the U.S. House of Representatives",
+        "question": "How many consecutive terms did Bernie Sanders serve in the House of Representatives?",
         "category": "Canidates",
         "1": "8",
         "2": "5",
@@ -40,72 +40,72 @@
         "1": "Donald Trump",
         "2": "Hillary Clinton",
         "3": "Bernie Sanders",
-        "4": "Ben Carson"
+        "4": "Ted Cruz"
     }, {
-        "question": "Wich of the nominees for presidency has be inducted in the WWE Hall of Fame?",
+        "question": "Which of the nominees for presidency has been inducted in the WWE Hall of Fame?",
         "category": "Canidates",
         "1": "Donald Trump",
         "2": "Hillary Clinton",
         "3": "Bernie Sanders",
         "4": "Ben Carson"
     }, {
-        "question": "Wich of the nominees for presidency Released their on vodka brand?",
+        "question": "Which of presidential candidate released their on vodka brand?",
         "category": "Canidates",
         "1": "Donald Trump",
         "2": "Hillary Clinton",
         "3": "Bernie Sanders",
         "4": "Ben Carson"
     }, {
-        "question": "Who has his own star on the Hollywood Walk of Fame which he received for the reality TV show The Apprentice",
+        "question": "Which candidate has their own star on the Hollywood Walk of Fame?",
         "category": "Canidates",
         "1": "Donald Trump",
         "2": "Hillary Clinton",
         "3": "Bernie Sanders",
         "4": "Ben Carson"
     }, {
-        "question": "Which one was known as a Cancer Survivor",
+        "question": "Which presidential candidate is a Cancer Survivor?",
         "category": "Canidates",
         "1": "Ben Carson",
         "2": "Hillary Clinton",
         "3": "Bernie Sanders",
         "4": "Donald Trump"
     }, {
-        "question": "When did Columbus discover America",
+        "question": "What year did Columbus discover America?",
         "category": "History",
         "1": "1492",
         "2": "1642",
         "3": "1596",
         "4": "1348"
     }, {
-        "question": "When was the 1773 Boston Tea Party",
+        "question": "When was the Boston Tea Party held?",
         "category": "History",
         "1": "1773",
         "2": "1689",
         "3": "1720",
         "4": "1780"
     }, {
-        "question": "When was the Boston Massacure",
+        "question": "What year was the Boston Massacare?",
         "category": "History",
         "1": "1770",
         "2": "1810",
         "3": "1780",
         "4": "1765"
     }, {
-        "question": "When was the Berlin Wall torn down?",
+        "question": "What year was the Berlin Wall torn down?",
         "category": "History",
         "1": "1989",
         "2": "1946",
         "3": "1996",
         "4": "1920"
     }, {
-        "question": "Who wrote the star spangled banner",
+        "question": "Who wrote the star spangled banner?",
         "category": "History",
         "1": "Francis Scott Key",
         "2": "Future",
-        "3": "21 Savage",
-        "4": "George Washington"
+        "3": "Beethoven",
+        "4": "Mozart"
     }, {
-        "question": "When was the emancipation proclamation signed ",
+        "question": "When was the emancipation proclamation signed?",
         "category": "History",
         "1": "January 1, 1863",
         "2": "March 6, 1850",
@@ -119,7 +119,7 @@
         "3": "1925",
         "4": "1940"
     }, {
-        "question": "Wich of these presidents have been impeached?",
+        "question": "Which of these presidents have been impeached?",
         "category": "History",
         "1": "Andrew Johnson",
         "2": "Donald Trump",
